cadd4f0491bb603aa644d319d0efa1cc77b1394a
Михаил Каташевцев <katash@ex.istu.edu>
2024-07-04 14:29:30 +0800

    process

--- rpd/models.py
+++ rpd/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from rpd.utils import TimestampsModel
 
+
 # Create your models here.
 class RPDFiles(TimestampsModel):
     title = models.CharField(max_length=100, verbose_name="Наименование файла")
     file = models.FileField(upload_to="uploads/%Y-%m-%d/", verbose_name="Файл РПД")
+
     def delete(self):
         self.is_deleted = True
         self.save()
+
+
+class Competence(models.Model):
+    code = models.IntegerField()
+    index = models.CharField(max_length=32)
+    content = models.CharField(max_length=2048)
+
+    class Meta:
+        db_table = "mleha_competences"
+        managed = False
