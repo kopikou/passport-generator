4de3aa6f6ca314a95276260a65e722425e027373
SkyFalI <alewaflamaster@gmail.com>
2024-07-04 19:26:17 +0800

    add file extension

--- client/src/pages/PlxUploadedView.vue
+++ client/src/pages/PlxUploadedView.vue
@@ -2,10 +2,23 @@
 
 import useMainStore from "stores/mainStore";
 import {storeToRefs} from "pinia";
+import {useQuasar} from "quasar";
 
 const mainStore = useMainStore();
-
 const {csrf} = storeToRefs(mainStore)
+
+const $q = useQuasar()
+
+function filefilter(files) {
+  return files.filter(file => file.name.slice(file.name.lastIndexOf(".")) == ".plx")
+}
+
+function onRejected(rejectedEntries) {
+  $q.notify({
+    type: 'negative',
+    message: `${rejectedEntries.length} файл не прошли проверку, загружайте только файла расширения .plx`,
+  })
+}
 
 </script>
 
@@ -13,11 +26,13 @@ const {csrf} = storeToRefs(mainStore)
 <div>
   <div class="q-pa-md">
     <q-uploader
-      label="Auto Uploader"
+      label="Загрузка файлов"
       auto-upload
       url="/api/upload/insert_file/"
       multiple
+      :filter="filefilter"
       :headers="[{name: 'X-CSRFToken', value: csrf}]"
+      @rejected="onRejected"
     />
   </div>
 </div>
