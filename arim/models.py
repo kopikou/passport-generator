85807988464f3dc1723c56c705e0333df90705c9
SkyFalI <alewaflamaster@gmail.com>
2024-08-12 16:51:29 +0800

    lines transfer ready

--- arim/models.py
+++ arim/models.py
@@ -9,14 +9,37 @@ class BoolChoice(TextChoices):
     f = 'f', "False"
 
 
+class Catadmission(models.Model):
+    class Meta:
+        db_table = "catadmission"
+        managed = False
+
+    yr = models.IntegerField()
+    abbr = models.CharField(max_length=10)
+
+
+class UchPlanKaf(models.Model):
+    class Meta:
+        db_table = "uchplan_kaf"
+        managed = False
+
+    ckaf2istu = models.IntegerField()
+    ckaf2rpgen = models.IntegerField()
+    name2rpgen = models.CharField(max_length=250)
+    ckaf2asp = models.IntegerField(null=True, blank=True)
+
+
 class UchPlanPlan(models.Model):
+    class Meta:
+        db_table = 'uchplan_plan'
+        managed = False
 
     species = models.CharField(max_length=4096, null=True, blank=True)
     studyprog = models.CharField(max_length=64, null=True, blank=True)
     studyform = models.CharField(max_length=16, null=True, blank=True)
     fullplanname = models.CharField(max_length=256, null=True, blank=True)
     name = models.CharField(max_length=256, null=True, blank=True)
-    kafcode = models.CharField(max_length=256, null=True, blank=True)
+    kafcode = models.ForeignKey("UchPlanKaf", on_delete=models.CASCADE, null=True, blank=True, db_column="kafcode")
     ckaf = models.IntegerField(null=True, blank=True)
     lastshifr = models.CharField(max_length=64, null=True, blank=True)
     abbrprofile = models.CharField(max_length=16, null=True, blank=True)
@@ -25,13 +48,63 @@ class UchPlanPlan(models.Model):
     gosdocument = models.IntegerField(null=True, blank=True)
     gostype = models.FloatField(null=True, blank=True)
     additiondate = models.CharField(max_length=16, null=True, blank=True)
-    cadmission = models.IntegerField(null=True, blank=True)
+    cadmission = models.ForeignKey("Catadmission", null=True, blank=True, on_delete=models.CASCADE, db_column="cadmission")
     fordel = models.CharField(max_length=1, choices=BoolChoice, default=BoolChoice.f)
     cperson = models.IntegerField(null=True, blank=True)
     cobrazstandart = models.IntegerField(null=True, blank=True)
     moved = models.CharField(max_length=1, choices=BoolChoice, default=BoolChoice.f)
     guid = models.CharField(max_length=64, null=True, blank=True)
 
+
+class UchPlanDiscpl(models.Model):
     class Meta:
-        db_table = 'uchplan_plan'
+        db_table = 'uchplan_discpl'
         managed = False
+
+    name = models.CharField(max_length=256)
+
+
+class UchPlanLines(models.Model):
+    class Meta:
+        db_table = "uchplan_lines"
+        managed = False
+
+    planid = models.ForeignKey("UchPlanPlan", on_delete=models.CASCADE, db_column="planid")
+    disid = models.ForeignKey("UchPlanDiscpl", on_delete=models.CASCADE, db_column="disid")
+    cperson = models.IntegerField(null=True, blank=True)
+    newdisid = models.CharField(max_length=32, null=True, blank=True)
+    iddis = models.CharField(max_length=32, null=True, blank=True)
+    kompetences = models.CharField(max_length=2048, null=True, blank=True)
+    mustbesdudied = models.IntegerField(null=True, blank=True)
+    hoursinzet = models.IntegerField(null=True, blank=True)
+    kafcode = models.ForeignKey("UchPlanKaf", on_delete=models.CASCADE, null=True, blank=True, db_column="kafcode")
+    ckaf = models.IntegerField(null=True, blank=True)
+    nocalccontrol = models.IntegerField(null=True, blank=True)
+    fordel = models.CharField(max_length=1, choices=BoolChoice, default=BoolChoice.f)
+    type = models.IntegerField(null=True, blank=True)
+    viewobject = models.IntegerField(null=True, blank=True)
+    viewpract = models.IntegerField(null=True, blank=True)
+
+
+class UchPlanSemestr(models.Model):
+    class Meta:
+        db_table = "uchplan_semester"
+        managed = False
+
+    planlineid = models.ForeignKey("UchPlanLines", on_delete=models.CASCADE, db_column="planlineid")
+    num = models.IntegerField()
+    lekc = models.IntegerField(null=True, blank=True)
+    lab = models.IntegerField(null=True, blank=True)
+    pr = models.IntegerField(null=True, blank=True)
+    srs = models.IntegerField(null=True, blank=True)
+    ekzhour = models.IntegerField(null=True, blank=True)
+    zet = models.IntegerField(null=True, blank=True)
+    ekz = models.CharField(max_length=1, choices=BoolChoice, null=True, blank=True)
+    zach = models.CharField(max_length=1, choices=BoolChoice, null=True, blank=True)
+    kp = models.CharField(max_length=1, choices=BoolChoice, null=True, blank=True)
+    kp_hour = models.IntegerField()
+    kr = models.CharField(max_length=1, choices=BoolChoice, null=True, blank=True)
+    kr_hour = models.IntegerField()
+    zacho = models.IntegerField()
+    eios = models.IntegerField()
+
--- rpd/management/commands/transfer_plx_to_ais.py
+++ rpd/management/commands/transfer_plx_to_ais.py
@@ -1,8 +1,9 @@
 from pprint import pprint
 
 from django.core.management import BaseCommand
+from django.db.models import Q
 
-from arim.models import UchPlanPlan
+from arim.models import UchPlanPlan, UchPlanLines, UchPlanDiscpl, UchPlanKaf
 from arim.services import AISServices
 from rpd.models import RPDFile, LinesData, PlanData
 
@@ -16,33 +17,82 @@ class Command(BaseCommand):
         # RPDFile.objects.filter(status=RPDFile.StatusChoice.accepted).update(status=RPDFile.StatusChoice.on_synchronize)
 
         for i in data:
-            plan_data = PlanData.objects.filter(file_id=i.id)
-            line_data = LinesData.objects.filter(synchronize=True, plan__file_id=i.id)
-            semester_data = LinesData.objects.filter(synchronize=True, plan__file_id=i.id)
+            plan_data = PlanData.objects.filter(file_id=i.id).values()
+            line_data = LinesData.objects.filter(synchronize=True, plan__file_id=i.id).values()
+            semester_data = LinesData.objects.filter(synchronize=True, plan__file_id=i.id).values()
 
             transfer_plan_data = {}
-            for i in plan_data:
+            for plan in plan_data:
                 transfer_plan_data = {
-                    "species": i.species,
-                    "studyprog": i.studyprog,
-                    "fullplanname": i.planname,
-                    "name": i.planname,
-                    "kafcode": i.kafcode,
-                    "lastshifr": i.lastshifr,
-                    "abbrprofile": i.abbrprofile,
-                    "startyear": i.startyear,
-                    "gosdate": i.gosdate,
-                    "gosdocument": i.gosdocument,
-                    "gostype": i.gostype,
+                    "species": plan['species'],
+                    "studyprog": plan['studyprog'],
+                    "fullplanname": plan['planname'],
+                    "name": plan['planname'],
+                    "kafcode": plan['kafcode'],
+                    "lastshifr": plan['lastshifr'],
+                    "abbrprofile": plan['abbrprofile'],
+                    "startyear": plan['startyear'],
+                    "gosdate": plan['gosdate'],
+                    "gosdocument": plan['gosdocument'],
+                    "gostype": plan['gostype'],
                     "fordel": 'f',
                 }
 
-            check = ""
 
-            plan_mira_id = 0  # берем мира ид если он Есть
+                uchplan, created = UchPlanPlan.objects.get_or_create(
+                    species=plan['species'],
+                    startyear=plan['startyear'],
+                    abbrprofile=plan['abbrprofile'],
+                    defaults=transfer_plan_data,
+                )
 
-            pprint(plan_data)
-            pprint(line_data)
-            pprint(semester_data)
+            query = Q()
+            for line in line_data:
+                query |= Q(name=line['dis'])
+
+            discpl_names = UchPlanDiscpl.objects.filter(query)
+            discpl_names = {f"{i['name']}": i for i in discpl_names.values()}
+
+            kaf_codes = UchPlanKaf.objects.all()
+            kaf_codes = {i['ckaf2rpgen']: i for i in kaf_codes.values()}
+
+            for line in line_data:
+
+                if not discpl_names.get(line['dis']):
+                    disid, created = UchPlanDiscpl.objects.get_or_create(
+                        name=line['dis']
+                    )
+                    disid = disid.id
+                else:
+                    disid = discpl_names.get(line['dis'])['id']
+
+                transfer_line_data = {
+                    "planid_id": uchplan.id,
+                    "disid_id": disid,
+                    "newdisid": line['newdisid'],
+                    "iddis": line['newdisid'],
+                    "kompetences": line['kompetences'],
+                    "mustbesdudied": line['mustbesdudied'],
+                    "hoursinzet": line['hoursinzet'],
+                    "kafcode_id": line['caf'],
+                    "ckaf": kaf_codes.get(line['caf'], {}).get("ckaf2istu"),
+                    "nocalccontrol": line['nocalccontrol'],
+                    "fordel": "f",
+                    "type": line['type'],
+                    "viewpract": line['viewpract'],
+                    "viewobject": line['viewobject'],
+                }
+
+                lines, created = UchPlanLines.objects.get_or_create(
+                    planid_id=uchplan.id,
+                    disid_id=transfer_line_data['disid_id'],
+                    defaults=transfer_line_data,
+                )
+
+                print(transfer_line_data)
+
+            # pprint(plan_data)
+            # pprint(line_data)
+            # pprint(semester_data)
 
             # RPDFile.objects.get(id=i.id).update(status=RPDFile.StatusChoice.finished)
