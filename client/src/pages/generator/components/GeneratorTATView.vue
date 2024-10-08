cc576ad637ba72931abafd691cfedfea5a6b91a6
SkyFalI <alewaflamaster@gmail.com>
2024-10-08 15:37:49 +0800

    maybe complete

--- client/src/pages/generator/components/GeneratorTATView.vue
+++ client/src/pages/generator/components/GeneratorTATView.vue
@@ -1,8 +1,40 @@
 <script setup lang="ts">
 
-import GeneratorTATItem from "pages/generator/components/TAT/GeneratorTATItem.vue";
 import GeneratorTATMarkItem from "pages/generator/components/TAT/GeneratorTATMarkItem.vue";
 import GeneratorTATZachItem from "pages/generator/components/TAT/GeneratorTATZachItem.vue";
+import useGeneratorViewStore from "stores/generatorViewStore";
+import {storeToRefs} from "pinia";
+import {computed} from "vue";
+import _ from "lodash";
+
+
+const generatorViewStore = useGeneratorViewStore();
+
+const{
+  semestersData,
+}=storeToRefs(generatorViewStore)
+
+const ekzCheck = computed(() => {
+  return _.some(semestersData.value, {'ekz': true})
+})
+
+const zachCheck = computed(() => {
+  return _.some(semestersData.value, {'zach': true})
+})
+
+const zachoCheck = computed(() => {
+  return _.some(semestersData.value, {'zacho': 1})
+})
+
+const kpCheck = computed(() => {
+  return _.some(semestersData.value, {'kp': true})
+})
+
+const krCheck = computed(() => {
+  return _.some(semestersData.value, {'kr': true})
+})
+
+
 </script>
 
 <template>
@@ -14,10 +46,10 @@ import GeneratorTATZachItem from "pages/generator/components/TAT/GeneratorTATZac
       bordered
       style="border-bottom: none;"
     >
-      <generator-t-a-t-mark-item title="Экзамен" type="ekz"/>
-      <generator-t-a-t-mark-item title="Дифференцированный зачет" type="zacho"/>
-      <generator-t-a-t-zach-item title="Зачет" type="zach"/>
-      <generator-t-a-t-mark-item title="Курсовая работа/проект" type="krkp"/>
+      <generator-t-a-t-mark-item v-if="ekzCheck" title="Экзамен" type="ekz"/>
+      <generator-t-a-t-mark-item v-if="zachoCheck" title="Дифференцированный зачет" type="zacho"/>
+      <generator-t-a-t-zach-item v-if="zachCheck" title="Зачет" type="zach"/>
+      <generator-t-a-t-mark-item v-if="kpCheck || krCheck" title="Курсовая работа/проект" type="krkp"/>
     </q-list>
   </div>
 </template>
