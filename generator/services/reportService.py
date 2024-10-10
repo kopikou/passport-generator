db70f272e1caf0f5915443316e4a0ca59f483a4a
SkyFalI <alewaflamaster@gmail.com>
2024-10-10 16:46:22 +0800

    indicators output complete

--- generator/services/reportService.py
+++ generator/services/reportService.py
@@ -17,14 +17,29 @@ class ReportService(object):
         doc = DocxTemplate(path)
 
         competences_sorted = sorted(data['planlines']['indicators'], key=lambda item: item['competence_index'])
-        competences_grouped = {key: list(items) for key, items in groupby(competences_sorted, key=lambda  item: item['competence_index'])}
+        competences_grouped = {key: list(items) for key, items in
+                               groupby(competences_sorted, key=lambda item: item['competence_index'])}
 
         competence = []
         for key, item in competences_grouped.items():
             competence.append({
                 "index": key,
-                "label": item[0]['competence'],
+                "content": item[0]['competence'],
                 "indicators": ", ".join([i['indicator_index'] for i in item]),
+            })
+
+        indicators = []
+        for item in data['planlines']['indicators']:
+            indicators.append({
+                'index': item['indicator_index'],
+                'content': item['indicator'],
+                'know': item['discipline_indicator'][0]['know'] if item['discipline_indicator'][0]['know'] else '',
+                'able': item['discipline_indicator'][0]['able'] if item['discipline_indicator'][0]['able'] else '',
+                'own': item['discipline_indicator'][0]['own'] if item['discipline_indicator'][0]['own'] else '',
+                'criteria': item['discipline_indicator'][0]['criteria'] if item['discipline_indicator'][0][
+                    'criteria'] else '',
+                'methods': item['discipline_indicator'][0]['methods'] if item['discipline_indicator'][0][
+                    'methods'] else '',
             })
 
         context = {
@@ -43,6 +58,7 @@ class ReportService(object):
             "person": data['person'],
             "person_name": CatPerson.objects.get(id=data['person']).name,
             "competence": competence,
+            "indicators": indicators,
         }
 
         doc.render(context)
