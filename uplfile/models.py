1d27247205464663d5922aebb76b8b679e64ef59
SkyFalI <alewaflamaster@gmail.com>
2024-10-30 13:42:02 +0800

    small changes

--- uplfile/models.py
+++ uplfile/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from generator.models import PlanLinesLink
+from rpd.models import DocumentsTypes, PlanData
 from rpd.utils import TimestampsModel
+
+
+class UploadFiles(TimestampsModel):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    title = models.TextField()
+    file = models.FileField(upload_to="uploads/files/%Y-%m-%d/")
+    type = models.ForeignKey(DocumentsTypes, on_delete=models.CASCADE)
+    rpd = models.ForeignKey(PlanData, on_delete=models.CASCADE)
+    line = models.ForeignKey(PlanLinesLink, on_delete=models.CASCADE, null=True)
