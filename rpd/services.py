74a0055095b1514e4cdb5735e392e21a3cd77a55
skyfall <skyfall@ex.istu.edu>
2025-03-17 10:09:25 +0800

    small fix

--- rpd/services.py
+++ rpd/services.py
@@ -201,8 +201,8 @@ class PLXParser:
             planData['igahourzet'] = float(child.attrib.get('ЗЕТвНеделю'))
             planData['semesteroncource'] = int(child.attrib.get('СеместровНаКурсе'))
             self.semesteroncource = int(child.attrib.get('СеместровНаКурсе'))
-            planData['gosdocument'] = float(child.attrib.get('НомерФГОС'))
-            planData['gostype'] = int(child.attrib.get('ТипГОСа'))
+            planData['gosdocument'] = int(child.attrib.get('НомерФГОС'))
+            planData['gostype'] = float(child.attrib.get('ТипГОСа'))
             planData['gosdate'] = datetime.fromisoformat((child.attrib.get('ДатаГОСа'))).strftime(
                 "%Y-%m-%d") if child.attrib.get('ДатаГОСа') else None
             planData['gostype'] = float(child.attrib.get('ТипГОСа'))
