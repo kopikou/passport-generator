f16a569be5ffe28ef8e1d01a5b1566ca36330470
Михаил Каташевцев <katash@ex.istu.edu>
2025-05-14 17:02:38 +0800

    fix

--- client/src/App.vue
+++ client/src/App.vue
@@ -90,9 +90,7 @@ onBeforeMount(async () => {
     <q-page-container class="container">
       <q-page style="overflow: hidden">
         <router-view v-slot="{ Component }">
-          <keep-alive>
             <component :is="Component" />
-          </keep-alive>
         </router-view>
       </q-page>
     </q-page-container>
