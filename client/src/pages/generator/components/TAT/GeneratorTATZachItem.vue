8db32619db11dc8dfd005d34c159565348ff8622
skyfall <skyfall@ex.istu.edu>
2025-05-28 09:23:00 +0800

    del a symbol

--- client/src/pages/generator/components/TAT/GeneratorTATZachItem.vue
+++ client/src/pages/generator/components/TAT/GeneratorTATZachItem.vue
@@ -137,7 +137,6 @@ watchEffect(() => {
               debounce="1000"
               @update:modelValue="saveData"
             />
-            a
             <q-input
               label="Пример задания"
               type="textarea"
