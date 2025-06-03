5e383eaf39229ffff0e4fd82954017fe5a214f32
skyfall <skyfall@ex.istu.edu>
2025-06-03 11:23:22 +0800

    save

--- generator/management/commands/add_default_num_in_tat.py
+++ generator/management/commands/add_default_num_in_tat.py
@@ -1,9 +1,10 @@
+from itertools import groupby
 from pprint import pprint
 
 from django.core.management import BaseCommand
 
 from generator.models import AdditionalInfo
-from rpd.models import LinesData
+from rpd.models import LinesData, SemesterData
 
 
 class Command(BaseCommand):
@@ -11,8 +12,35 @@ class Command(BaseCommand):
 
         data = AdditionalInfo.objects.filter(type='tat').select_related("planlineslink")
 
-        lines = LinesData.objects
+        semesters = SemesterData.objects.filter(planlineid__in=[i.planlineslink.planlines_id for i in data]).values()
+        semesters_sorted = sorted(semesters, key=lambda x: x['planlineid_id'])
+        semesters_by_planlineid = {key: list(items) for key, items in groupby(semesters_sorted, key=lambda x: x['planlineid_id'])}
 
+        res = []
         for i in data:
-            pprint(i.value)
-            pprint(i.planlineslink.id)
+            for j in i.value:
+                semester = semesters_by_planlineid.get(i.planlineslink.planlines_id, {})
+                if j['type'] != 'krkp':
+                    filtered_semesters = list(filter(lambda x: x.get(j['type'], False), semester))
+                else:
+                    filtered_semesters = list(filter(lambda x: x.get('kp', False) or x.get('kr', False), semester))
+
+                for q in filtered_semesters:
+                    res.append({
+                        "value": {
+                            **j,
+                            "num": q['num'],
+                        },
+                        "id": i.id,
+                    })
+
+                # if not filtered_semesters:
+                #     pprint(j)
+                #     pprint(i.planlineslink)
+        pprint(res)
+
+
+
+
+
+
