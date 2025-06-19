d94b9e064edc634e4b64b55bb74eea4eb3ccf672
Михаил Каташевцев <katash@ex.istu.edu>
2025-06-19 11:45:49 +0800

    fix

--- generator/management/commands/generate_accepted_planlines.py
+++ generator/management/commands/generate_accepted_planlines.py
@@ -1,4 +1,5 @@
 from django.core.management import BaseCommand
+from django.db.models import Q
 from tqdm import tqdm
 
 from generator.models import PlanLinesLink
@@ -9,9 +10,9 @@ from generator.services.generator_service import GeneratorService
 class Command(BaseCommand):
     def handle(self, *args, **options):
         links = PlanLinesLink.objects.filter(
+            Q(last_accepted_file__isnull=True) | Q(last_accepted_file__exact=''),
             status=PlanLinesLink.StatusChoices.accepted,
             uploaded_directly=False,
-            last_accepted_file__isnull=True
         )
         pbar = tqdm(links)
         for l in pbar:
