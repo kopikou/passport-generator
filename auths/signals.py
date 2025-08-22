b80f4e58a0dadd8ac8ebfbf3bda4f4e38e91fad5
skyfall <skyfall@ex.istu.edu>
2025-08-22 09:47:45 +0800

    small change signal

--- auths/signals.py
+++ auths/signals.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import user_logged_in
 from django.dispatch import receiver
 
-from arim.models import CatPerson
+from arim.models import CatPerson, CatStud
 from auths.models import Permissions
 
 
@@ -10,17 +10,27 @@ def user_logged_in_callback(sender, request, user, **kwargs):
 
        mira_data = CatPerson.objects.get(id=user.esiauser.mira_id)
 
-       if mira_data:
+       if 'student' in user.esiauser.types:
+              mira_data = CatStud.objects.get(id=user.esiauser.mira_id)
               name = mira_data.name.split(' ')
 
               user.userprofile.name = mira_data.name
               user.userprofile.middle_name = ' '.join(name[2:])
               user.userprofile.mira_id = mira_data.id
 
-              if 'student' in user.esiauser.types:
-                     user.is_student = True
-              if 'employee' in user.esiauser.types:
+              user.userprofile.is_student = True
+              user.userprofile.save()
+
+       elif 'employee' in user.esiauser.types:
+              name = mira_data.name.split(' ')
+
+              user.userprofile.name = mira_data.name
+              user.userprofile.middle_name = ' '.join(name[2:])
+              user.userprofile.mira_id = mira_data.id
+
+              if 'teacher' in user.esiauser.types:
                      user.is_teacher = True
-                     user.userprofile.permissions = [Permissions.can_edit_rpd, Permissions.can_use_generator]
+
+              user.userprofile.permissions = [Permissions.can_edit_rpd, Permissions.can_use_generator]
 
               user.userprofile.save()
