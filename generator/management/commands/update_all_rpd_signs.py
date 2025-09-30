0b556520cdff1b5013021430932a19e8a9ab293b
provotorovva <provotorovva@ex.istu.edu>
2025-09-30 11:45:35 +0800

    fix

--- generator/management/commands/update_all_rpd_signs.py
+++ generator/management/commands/update_all_rpd_signs.py
@@ -15,9 +15,12 @@ class Command(BaseCommand):
         print(f"Найдено {total_count} записей PlanLinesLink")
 
         for index, planline_instance in enumerate(tqdm(planlines, total=total_count, desc="Обновление подписей РПД"), 1):
-            sig_id, sig_string = GeneratorService.get_sig_id_string(planline_instance)
+            current_sig = planline_instance.last_accepted_sig
+            current_sig_id = planline_instance.last_accepted_sig_id
+            if not current_sig or not current_sig_id:
+                sig_id, sig_string = GeneratorService.get_sig_id_string(planline_instance)
 
-            if sig_id and sig_string:
-                planline_instance.last_accepted_sig_id = sig_id
-                planline_instance.last_accepted_sig = sig_string
-                planline_instance.save(update_fields=['last_accepted_sig_id', 'last_accepted_sig'])
+                if sig_id and sig_string:
+                    planline_instance.last_accepted_sig_id = sig_id
+                    planline_instance.last_accepted_sig = sig_string
+                    planline_instance.save(update_fields=['last_accepted_sig_id', 'last_accepted_sig'])
